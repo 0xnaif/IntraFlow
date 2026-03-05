66e4c23db73158b689deb11d1ac9c4e4906dc699
Naif <niefm1422@gmail.com>
2026-03-05 04:35:02 +0300

    feat(ui): introduce application layout with navbar and footer partials

---
+++ IntraFlow.Web/Views/Shared/_Navbar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IntraFlow.Web.Views.Shared
+{
+    public class _NavbarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
